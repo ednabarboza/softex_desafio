fdab1ccac0deca8670b920669ae3db5f034525d7
Diego Augusto <DiegoprogBr@hotmail.com>
2022-11-23 17:37:46 -0300

    add empregado.js

---
+++ js/empregado.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const database = require('./db');
+
+
+const Empregado = database.define('Empregado',
+{
+    Matricula:
+    {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    Nome:
+    {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    Data_Nasc:
+    {
+        type: Sequelize.DATE
+    },
+    Sexo:
+    {
+        type: Sequelize.CHAR
+    },
+    Salario:
+    {
+        type: Sequelize.FLOAT
+    },
+    Supervisor:
+    {
+        type: Sequelize.STRING
+    },
+    Depto:
+    {
+        type: Sequelize.STRING
+    }
+}
+);
+
+await Empregado.sync();
+
+module.exports = Empregado;
