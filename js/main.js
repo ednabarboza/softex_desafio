8cbadf5704307ada12acfd340f87d3055733e65b
Diego Augusto <DiegoprogBr@hotmail.com>
2022-11-23 21:51:24 -0300

    add rota get

--- js/main.js
+++ js/main.js
@@ -11,28 +11,11 @@
     db.connect();
     console.log('Starting to connect to MySQL...');
 
-    // O código abaixo é só para teste
-    const funcionarios = [{nome:'Zezinho',nasc:'1995-03-01',sexo:'M',salario:'2000',supervisor:'Ruy',depto:'backend'},
-		                  {nome:'Astrogildo',nasc:'1998-10-15',sexo:'M',salario:'3000',supervisor:'Rey',depto:'frontend'},
-                          {nome:'Beltrana',nasc:'2005-09-19',sexo:'F',salario:'900',supervisor:'Ray',depto:'estagio'},
-                          {nome:'Fulana',nasc:'2000-01-07',sexo:'F',salario:'1200',supervisor:'Rey',depto:'frontend'},
-                          {nome:'Sicrana',nasc:'2001-11-05',sexo:'F',salario:'3200',supervisor:'Ruy',depto:'backend'}];
-    
-    funcionarios.forEach(async (funcionario) => {
-        await db.insert(funcionario);
+    app.get('/', (req, res) => {
+        res.render('index.html');
     });
-    
-    const func = await db.select();
-    console.log(func);
 
-    await db.update(1, {nome:'Zé',nasc:'1998-05-20',sexo:'M',salario:'3500',supervisor:'Rey',depto:'frontend'});
-
-    await db.erase(2);
-
-    const f = await db.select();
-    console.log(f);
-
-    // Teste
+    /* Teste
     await empregado.create(
         {
             Matricula: 1,
@@ -44,5 +27,5 @@
             Depto: "Back-End"
         }
     )
-
+    */
 })();
